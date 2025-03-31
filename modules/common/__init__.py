f0c8bfd35c688c0daa6086178b5c83eae19c6376
servais1983 <stservais0409@gmail.com>
2025-03-31 14:24:51 +0200

    Ajout du fichier d'initialisation pour le module commun

---
+++ modules/common/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module commun pour les fonctionnalités partagées
+
+Ce module contient des composants et utilitaires utilisés
+par les autres modules de collecte.
+"""
