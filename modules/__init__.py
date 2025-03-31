471273fa651dc9be84b101b418428574665f0833
servais1983 <stservais0409@gmail.com>
2025-03-31 14:24:59 +0200

    Ajout du fichier d'initialisation pour le dossier modules principal

---
+++ modules/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+Modules de collecte de preuves forensiques
+
+Ce paquet contient les modules spécialisés pour la collecte
+de différents types de preuves forensiques.
+"""
